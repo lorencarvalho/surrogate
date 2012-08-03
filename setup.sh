9cfb7f898d32b80fe1265e7831b3cae1acc109e0
Loren Carvalho <comradeloren@gmail.com>
2012-08-02 21:16:12 -0500

    checking out

--- setup.sh
+++ setup.sh
@@ -6,13 +6,15 @@ if [ `whoami` != root ]; then
     exit
 fi
 
+source surrogate
 
 [[ ! -d $backup_directory  ]] && { mkdir -p $backup_directory ; }
 [[ ! -d $mysql_sanity_path  ]] && { mkdir -p $mysql_sanity_path ; }
 [[ ! -d $mysql_log_path  ]] && { mkdir -p $mysql_log_path ; }
 [[ ! -d $mysql_data_path  ]] && { mkdir -p $mysql_data_path ; }
+[[ ! -d log  ]] && { mkdir -p log ; }
 
-chown -R mysql:mysql $backup_directory
-chown -R mysql:mysql $mysql_sanity_path
-chown -R mysql:mysql $mysql_log_path
-chown -R mysql:mysql $mysql_data_path
+chown -R mysql $backup_directory
+chown -R mysql $mysql_sanity_path
+chown -R mysql $mysql_log_path
+chown -R mysql $mysql_data_path
