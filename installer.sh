04c4877d78c29966adc8e1aed53ea0ca2916a4e0
SGE Infrastructure <infrastructure@sleepygiant.com>
2012-08-16 02:31:46 -0500

    more installer fun

--- installer.sh
+++ installer.sh
@@ -16,30 +16,46 @@ EOF
 exit 1
 }
 
+function fakeprog() {
+  /bin/echo -n "---" && sleep 2
+}
+
 echo "installing surrogate"
 
-# check and create dirs
+# hey bro, are you root?
 if [ "$(whoami)" != "root" ]; then
   usage
   exit 1
 fi
 
+echo "["`fakeprog`
+
 if ! [ -d "$confdir" ]; then
   mkdir -p $confdir
 fi
+
+fakeprog
 
 if ! [ -d "$libdir" ]; then
   mkdir -p $libdir
 fi
 
+fakeprog
+
 # install the software
 rsync -a ./files/ $libdir/
+
+fakeprog
 
 # configure it
 cp -R $libdir/conf/* $confdir/
 
+fakeprog
+
 # symlink to include in path
 rm /usr/local/bin/surrogate
 ln -s /usr/local/share/surrogate/surrogate /usr/local/bin/surrogate
+
+/bin/echo -n "]"
 
 echo "installation complete!"
